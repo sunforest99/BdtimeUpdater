4720693fd7064a5de9d105e1d71a2b4e326e896f
lazyevilsquid <taeyang9300@daum.net>
2017-11-29 13:58:24 +0900

    first commit

---
+++ BdtimeUpdater/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace BdtimeUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 194);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+    }
+}
+
---
+++ BdtimeUpdater/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using agi = HtmlAgilityPack;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace BdtimeUpdater
+{
+    public partial class Form1 : Form
+    {
+        // -----
+        public int nTrNum = 5;
+        string sWebSite = "http://lazytitan.dothome.co.kr/BdBossPhp/";
+        public bool[] bCheck = new bool[4];
+
+        // ----- HTML
+        string[] sBosshtml = new string[4];     // 0 크자카,1 누배르,2 쿠툼,3 카란다
+
+        // ----- Parsing
+        agi.HtmlNode bossname;
+        agi.HtmlNode bossdate;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Thursday)
+            {
+                ResetBossTime();
+                return;
+            }
+            var sFirst = "//*[@id='blackMain']//*[@id='powerbbsBody']/table/tr/td/div/table/tr/td/table/tr[3]/td/table/tbody/tr[";
+            var sLast = "]/td[2]/a";
+
+            var html = @"http://www.inven.co.kr/board/powerbbs.php?come_idx=4224";     // 웹 주소
+
+            agi.HtmlWeb web = new agi.HtmlWeb();    // 걍 선언
+            for (; ; nTrNum++)
+            {
+                if (nTrNum > 51)
+                {
+                    nTrNum = 5;
+                    bCheck[0] = false;
+                    bCheck[1] = false;
+                    bCheck[2] = false;
+                    bCheck[3] = false;
+                }
+
+                else
+                {
+                    var htmlDoc = web.Load(html);           // 웹로드
+
+                    bossname = htmlDoc.DocumentNode.SelectSingleNode(sFirst + nTrNum + sLast);       // 테그 찾기     마지막 태그 td[4] = 날짜
+
+                    bossdate = htmlDoc.DocumentNode.SelectSingleNode(sFirst + nTrNum + "]/td[4]");       //str.Contains
+
+                    FindBossName();
+                }
+            }
+        }
+
+        void FindBossName()
+        {
+            if (bossname.OuterHtml.Contains("크자카") && !bCheck[0])
+            {
+                if (bossname.InnerText.IndexOf(':') == -1)
+                {
+                    Console.Write(bossdate.InnerText.Remove(5));
+                    sBosshtml[0] = bossdate.InnerText.Remove(5);
+
+                    HttpGet(sWebSite + "kzarka.php?kzarka='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[0] + "'");
+                }
+                else
+                {
+                    Console.WriteLine(CatchNum());
+                    sBosshtml[0] = bossname.InnerText.Substring(bossname.InnerText.IndexOf(':') - 2, 5).Replace(';', '0');
+
+                    HttpGet(sWebSite + "kzarka.php?kzarka='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[0] + "'");
+                }
+                bCheck[0] = true;
+            }
+            if (bossname.OuterHtml.Contains("누베르") && !bCheck[1])
+            {
+                if (bossname.InnerText.IndexOf(':') == -1)
+                {
+                    Console.Write(bossdate.InnerText.Remove(5));
+                    sBosshtml[1] = bossdate.InnerText.Remove(5);
+
+                    HttpGet(sWebSite + "nouver.php?nouver='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[1] + "'");
+                }
+                else
+                {
+                    Console.WriteLine(CatchNum());
+                    sBosshtml[1] = bossname.InnerText.Substring(bossname.InnerText.IndexOf(':') - 2, 5).Replace(';', '0');
+
+                    HttpGet(sWebSite + "nouver.php?nouver='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[1] + "'");
+                }
+                bCheck[1] = true;
+            }
+            if (bossname.OuterHtml.Contains("쿠툼") && !bCheck[2])
+            {
+                if (bossname.InnerText.IndexOf(':') == -1)
+                {
+                    sBosshtml[2] = bossdate.InnerText.Remove(5);
+
+                    HttpGet(sWebSite + "kutum.php?kutum='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[2] + "'");
+                }
+                else
+                {
+                    Console.WriteLine(CatchNum());
+                    sBosshtml[2] = bossname.InnerText.Substring(bossname.InnerText.IndexOf(':') - 2, 5).Replace(';', '0');
+
+                    HttpGet(sWebSite + "kutum.php?kutum='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[2] + "'");
+                }
+                bCheck[2] = true;
+            }
+            if (bossname.OuterHtml.Contains("카란다") && !bCheck[3])
+            {
+                if (bossname.InnerText.IndexOf(':') == -1)
+                {
+                    sBosshtml[3] = bossdate.InnerText.Remove(5);
+
+                    HttpGet(sWebSite + "karanda.php?karanda='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[3] + "'");
+                }
+                else
+                {
+                    sBosshtml[3] = bossname.InnerText.Substring(bossname.InnerText.IndexOf(':') - 2, 5).Replace(';', '0');
+
+                    HttpGet(sWebSite + "karanda.php?karanda='" + DateTime.Now.ToString("yyyy-MM-dd") + " " + sBosshtml[3] + "'");
+                }
+                bCheck[3] = true;
+            }
+            label1.Text = bossname.OuterHtml + "\n\n";// + bossdate.OuterHtml;   // 테그 가져오기
+            nTrNum++;
+            return;
+        }
+
+        void ResetBossTime()
+        {
+            HttpGet(sWebSite + "Inspection.php?Reset='" + DateTime.Now.ToString("yyyy-MM-dd") + " 10:00:00'");
+        }
+
+        string CatchNum()
+        {
+            string sGetText;
+            sGetText = Regex.Replace(bossname.InnerText, @"/D", "");        // 숫자만 제외하고 다 없에버리기
+            return sGetText;
+        }
+
+        private void HttpGet(string urlStr)
+        {
+            WebClient webcli = new WebClient();
+
+            webcli.DownloadString(urlStr);
+        }
+    }
+}
